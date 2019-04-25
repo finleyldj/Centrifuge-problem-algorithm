732e7b88a7ddccd2bf450944204cac6c5f52bb56
Finn Draffin <finn.draffin@gmail.com>
2019-04-25 21:16:00 +0100

    broken help from reddit

--- IA.py
+++ IA.py
@@ -1,41 +1,25 @@
 #!/usr/bin/env python3
 import math 
 n = int(input('How many slots are there? '))
-s=n
-# A function to print all prime factors of  
-# a given number n 
-def primeFactors(n):  
-    # print the number of two's that divide n 
+s=[]
+s.append(n)
+# A function to print the prime factors of n 
+def primeFactors(s):  
+    # reducing n to an odd number 
     while n % 2 == 0: 
-       yield (int(2)), 
-       n = n / 2
-          
-    # n must be odd at this point 
-    # so a skip of 2 ( i = i + 2) can be used 
+       return (int(2)), 
+       s = n / 2
+           
     for i in range(3,int(math.sqrt(n))+1,2): 
           
-        # while i divides n , print i ad divide n 
+        # adding values of n to set s
         while n % i== 0: 
-            yield(int(i)), 
-            n = n / i 
-              
-    # Condition if n is a prime 
-    # number greater than 2 
+             c=int(n/i)
+             s.append(c)        
+    # Where n is a prime 
+    # > 2
     if n > 2: 
-        yied (int(n))	
-for n in primeFactors(n):
-	print(n)
-try:
-	pf_gen=primeFactors(n)
-	next (pf_gen)
-except TypeError:
-	pass
-#Function which prints all of the possible sums of the prime factors of n        
-#def sumofprimes(n):
-	#while s<=n:
-		#yield (s+)
-		
-#k= int(input('How many test tubes do you have?'))
-#if k==n:
-	#print ('This is balanced!')
+        pass	
+primeFactors(n)
+print (s)
         
