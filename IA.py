235a6fd7434df3e4c91d16e3a3d3adac9a1e2662
Finn Draffin <finn.draffin@gmail.com>
2019-04-26 20:52:28 +0100

    non-functional with all combinations

--- IA.py
+++ IA.py
@@ -2,27 +2,70 @@
 import math 
 n = int(input('How many slots are there? '))
 s=[]
-k=[]
+p=[]
+f=False
+l=False
 # A function to print the prime factors of n 
-def primeFactors(n):
-	i=1
-	while(i<=n):
-	    k=0
-	    if(n%i==0):
-	        j=1
-	        while(j<=i):
-	            if(i%j==0):
-	                q=q+1
-	            j=j+1
-	        ifqk==2):
-	            s.append(i)
-	    i=i+1	
-primeFactors(n)
+def prime_factors(n):
+	i = 2
+	while i * i <= n:
+		if n % i:
+			i += 1
+		else:
+			n //= i
+			s.append(i)
+	if n > 1:
+		s.append(n)
+	return s
+prime_factors(n)
 for x in range(len(s)):
 	print (s[x])       
 #Function which prints all of the possible sums of the prime factors of n        
-def primeSum(n):
-	g=1
-	while g<=len(s):
-		d=s[(len(s))-(len(s)-g)]
-		k.append(d)
+k=int(input('How many test tubes do you have? '))
+#lines 23-57 have been adapted from source code by stack overflow user "Jonathan"
+def check_sum(s, n):
+	sums1, sums2 = [], []
+	s1 = s[:len(s) // 2]
+	s2 = s[len(s) // 2:]
+
+	for sums, s_ in ((sums1, s1), (sums2, s2)):
+		for number in s_:
+			for sum_ in sums[:]:
+				sums.append(sum_ + number)
+
+			sums.append(number)
+
+	for sum_ in sums1:
+		if n - sum_ in sums2:
+			return True  
+
+def check_sum_holes(s,n):
+	sumzs1, sumzs2 = [], []
+	s3 = s[:len(s) // 2]
+	s4 = s[len(s) // 2:]
+
+	for sumzs, sz_ in ((sumzs1, s3), (sumzs2, s4)):
+		for number in sz_:
+			for sumz_ in sumzs[:]:
+				sumzs.append(sumz_ + number)
+
+			sumzs.append(number)
+
+	for sumz_ in sumzs1:
+		if n==n-k:
+			return True
+		if n - sumz_ in sumzs2:
+			return True	  
+check_sum(s,k)
+check_sum_holes(s,k)
+def check_balance (s,k):
+	check_sum(s,k)
+	check_sum_holes(s,k)
+	if check_sum(s,k) and check_sum_holes(s,k):
+		print("This is a balanced configuration")
+	else:
+		print("You cannot balance this centrifuge with " + str(k) + " test tubes")
+check_balance(s,k)		
+
+
+  
