564bd78edc7d4e0d8e88c33fe483672d5aeb9882
Finn Draffin <finn.draffin@gmail.com>
2019-04-26 21:37:09 +0100

    endless loop

--- IA.py
+++ IA.py
@@ -3,8 +3,7 @@ import math
 n = int(input('How many slots are there? '))
 s=[]
 p=[]
-f=False
-l=False
+f=[]
 # A function to print the prime factors of n 
 def prime_factors(n):
 	i = 2
@@ -23,39 +22,64 @@ for x in range(len(s)):
 #Function which prints all of the possible sums of the prime factors of n        
 k=int(input('How many test tubes do you have? '))
 #lines 23-57 have been adapted from source code by stack overflow user "Jonathan"
-def check_sum(s, n):
-	sums1, sums2 = [], []
-	s1 = s[:len(s) // 2]
-	s2 = s[len(s) // 2:]
+def check_sum(s, k):
+	g = 1
+	c=1
+	t=1
+	z=1
+	l=1
+	while c <=len(s):
+		while g < k:
+			while t < k:
+				t=s[c]*g
+				f.append(t)
+	while c <= len(s):
+		while z<(len(s)-1):
+			while g < k:
+				while t < k:
+					t=s[c]*g+s[c+z]
+					f.append(t)
+	while c<= len(s):
+		while z<(len(s)-1):
+			while g < k:
+				while t < k:
+					while l<g:		
+						t=s[c]*g+s[c+z]*l
+						f.append(t)
+	if k in s:
+		return True
+	else:
+		return False				
 
-	for sums, s_ in ((sums1, s1), (sums2, s2)):
-		for number in s_:
-			for sum_ in sums[:]:
-				sums.append(sum_ + number)
+def check_sum_holes(s,k):
+	g = 1
+	c=1
+	t=1
+	z=1
+	l=1
+	while c<=len(s):
+		while g < (n-k):
+			while t < (n-k):
+				t=s[c]*g
+				f.append(t)
+	while c <= len(s):
+		while z<(len(s)-1):
+			while g < (n-k):
+				while t < (n-k):
+					t=s[c]*g+s[c+z]
+					f.append(t)
+	while c<= len(s):
+		while z<(len(s)-1):
+			while g < (n-k):
+				while t < (n-k):
+					while l<g:		
+						t=s[c]*g+s[c+z]*l
+						f.append(t)
+	if n-k in s:
+		return True
+	else:
+		return False	
 
-			sums.append(number)
-
-	for sum_ in sums1:
-		if n - sum_ in sums2:
-			return True  
-
-def check_sum_holes(s,n):
-	sumzs1, sumzs2 = [], []
-	s3 = s[:len(s) // 2]
-	s4 = s[len(s) // 2:]
-
-	for sumzs, sz_ in ((sumzs1, s3), (sumzs2, s4)):
-		for number in sz_:
-			for sumz_ in sumzs[:]:
-				sumzs.append(sumz_ + number)
-
-			sumzs.append(number)
-
-	for sumz_ in sumzs1:
-		if n==n-k:
-			return True
-		if n - sumz_ in sumzs2:
-			return True	  
 check_sum(s,k)
 check_sum_holes(s,k)
 def check_balance (s,k):
